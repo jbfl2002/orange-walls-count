4d6f7c4733ae11bc20b66eaf85c274d3744d4a04
jbfl2002 <jose_3127210173@utd.edu.mx>
2024-06-17 11:41:05 -0600

    pastaavace

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -6,6 +6,9 @@ import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
     const {data: user} = useGetIdentity <User>()
+    
+    
+
     return (
         <>
         <Popover
@@ -15,6 +18,10 @@ const CurrentUser = () => {
         overlayStyle={{ zIndex: 999}}
         >
            <CustomAvatar />
+             name={user?.name}
+             src={user?.avatarUrl}
+             size="default"
+             style={{ cursor: 'pointer'}}
         </Popover>
         </>
     )
